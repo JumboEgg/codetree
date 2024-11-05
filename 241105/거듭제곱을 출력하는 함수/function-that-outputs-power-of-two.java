cccb8f7926047e8f9804447cddcc96f105fe8c58
JumboEgg <102843250+JumboEgg@users.noreply.github.com>
2024-11-05 16:41:57 +0900

    Title: 거듭제곱을 출력하는 함수, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241105/거듭제곱을 출력하는 함수/function-that-outputs-power-of-two.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        System.out.println((int) Math.pow(n, m));
+    }
+}
